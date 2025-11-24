be7430d202262fcfc32c36f0131d4eb459e6ce7c
KSourav323 <souravcndtl@gmail.com>
2025-11-24 23:43:01 +0530

    robots-and-sitemap

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://utillect.vercel.app", // change to your domain
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
